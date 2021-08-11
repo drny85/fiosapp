b81134aea08b942208220b346e4979399416b017
drny85 <robertm3lendez@gmail.com>
2021-08-10 23:12:44 -0400

    chanegs3

---
+++ hooks/useTodayNotes.js
@@ -0,0 +1,19 @@
+import { useEffect, useContext } from 'react';
+import authContext from '../context/auth/authContext';
+import notesContext from '../context/notes/notesContext';
+import moment from 'moment';
+
+export const useTodayNotes = (startDate = new Date(), endDate = new Date()) => {
+	const { notes, getNotes } = useContext(notesContext);
+	const { user } = useContext(authContext);
+
+	useEffect(() => {
+		getNotes(user?.id);
+	}, [user, notes.length]);
+
+	return notes.filter(
+		(n) =>
+			moment(n.addedOn).isAfter(moment(startDate).startOf('day')) &&
+			moment(n.addedOn).isBefore(moment(endDate).endOf('day'))
+	);
+};
--- screens/notes/Notes.jsx
+++ screens/notes/Notes.jsx
@@ -8,6 +8,7 @@ import ScreenView from '../ScreenView'
 import { Ionicons } from '@expo/vector-icons'
 import InputTextField from '../../components/InputTextField'
 import moment from 'moment'
+import { useTodayNotes } from '../../hooks/useTodayNotes'
 
 const NoteCard = ({ note }) => {
     return (
@@ -33,8 +34,11 @@ const NoteCard = ({ note }) => {
 const Notes = () => {
     const { user } = useContext(authContext)
     const [visible, setVisible] = useState(false)
-    const { notes, getNotes, addNote } = useContext(notesContext)
+    const { addNote } = useContext(notesContext)
     const [noteData, setNoteData] = useState('')
+
+    const notes = useTodayNotes()
+
 
     const saveNote = async () => {
         if (noteData.length < 5) {
@@ -54,16 +58,18 @@ const Notes = () => {
 
     }
 
-    useEffect(() => {
-        getNotes(user?.id)
-    }, [user])
     return (
         <ScreenView>
             <ButtonsTop canGoBack={false} iconRightName='plus' onPress={() => setVisible(true)} />
 
             <View style={styles.view}>
                 <Text style={{ ...FONTS.h4, textAlign: 'center' }}>Today's Notes</Text>
-                <FlatList data={notes} keyExtractor={item => item.id} renderItem={({ item }) => <NoteCard note={item} />} />
+                {notes.length > 0 ? (
+                    <FlatList data={notes} keyExtractor={item => item.id} renderItem={({ item }) => <NoteCard note={item} />} />
+                ) : <View style={{ height: SIZES.height * 0.8, justifyContent: 'center', alignItems: 'center' }}>
+                        <Text style={{ ...FONTS.body4 }}>No Notes</Text>
+                    </View>}
+
             </View>
 
             <Modal animationType='slide' visible={visible}>
@@ -72,7 +78,7 @@ const Notes = () => {
                         <Ionicons name='close' size={26} color={COLORS.black} />
                     </TouchableOpacity>
                     <View style={{ marginTop: 80, width: SIZES.width * 0.95, alignSelf: 'center' }}>
-                        <InputTextField multiline={true} placeholder='Type your note' value={noteData} onChangeText={text => setNoteData(text)} />
+                        <InputTextField multiline={true} autoCorrect={false} placeholder='Type your note' value={noteData} onChangeText={text => setNoteData(text)} />
                     </View>
                     <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', marginTop: 20 }}>
                         <TouchableOpacity style={{ paddingHorizontal: 25, paddingVertical: 15, justifyContent: 'center', width: SIZES.width / 3, alignItems: 'center', borderRadius: SIZES.radius * 3, alignSelf: 'center', shadowOffset: { width: 4, height: 6 }, backgroundColor: COLORS.white }} onPress={() => setNoteData('')}>
